d58413952e2fcca450df6db942865cab4dff77aa
Enzo Fet <enzofet1998@gmail.com>
2025-10-24 18:12:10 -0300

    alo

--- js/home.js
+++ js/home.js
@@ -1,11 +1,12 @@
 const images = document.querySelectorAll(".carrousel-images img");
+const frontImage = document.querySelector("#img frente")
 let contador = 0;
 const flechaPrev = document.getElementsByClassName("flecha-anterior")[0];
 const flechaNext = document.getElementsByClassName("flecha-siguiente")[0];
 const totalImagenes = images.length;
-function retrocederImagen(){
-    let imagen = images[contador].id("imagen-frente"); 
+function retrocederImagen() {
+  
+  contador++;
 }
 
 flechaPrev.addEventListener("click", retrocederImagen);
-
