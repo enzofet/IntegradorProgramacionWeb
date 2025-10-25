d9250b6f5457ca4a7247db9c71a987353099f7fd
Enzo Fet <enzofet1998@gmail.com>
2025-10-25 18:33:23 -0300

    Pude hacer avanzar el carrousel pa un lado y agregué la ventana de inscripcion

--- js/home.js
+++ js/home.js
@@ -1,12 +1,44 @@
-const images = document.querySelectorAll(".carrousel-images img");
-const frontImage = document.querySelector("#img frente")
-let contador = 0;
+const imagesCarrousel = document.querySelectorAll(".imagen");
+const containerCarrousel = document.querySelector("#imagenes");
+
+
+
+containerCarrousel.classList.add("imagen-frente");
+containerCarrousel.firstElementChild.classList.add("imagen-frente");
 const flechaPrev = document.getElementsByClassName("flecha-anterior")[0];
 const flechaNext = document.getElementsByClassName("flecha-siguiente")[0];
-const totalImagenes = images.length;
+
+flechaNext.addEventListener("click", avanzarImagen);
+flechaPrev.addEventListener("click", retrocederImagen);
+
+let contador = 0;
+
 function retrocederImagen() {
   
-  contador++;
 }
 
-flechaPrev.addEventListener("click", retrocederImagen);
+function avanzarImagen() {
+    const firstImagen = containerCarrousel.firstElementChild;
+    containerCarrousel.firstElementChild.classList.remove("imagen-frente");
+    containerCarrousel.removeChild(firstImagen);
+    containerCarrousel.appendChild(firstImagen);
+    containerCarrousel.firstElementChild.classList.add("imagen-frente");
+}
+
+/*function avanzarImagen() {
+  if(contador != imagesCarrousel.length -1){
+    const firstImagen = containerCarrousel.firstElementChild;
+    imagesCarrousel[contador].classList.remove("imagen-frente");
+    containerCarrousel.removeChild(firstImagen);
+    containerCarrousel.appendChild(firstImagen);
+    contador++;
+    imagesCarrousel[contador].classList.add("imagen-frente");
+  } else {
+    contador = 0;
+    moverElemento();
+  }
+}*/
+
+
+
+
